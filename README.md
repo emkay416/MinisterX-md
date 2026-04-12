f04e76435d6ebfd0f3e766ff4c207a42899b371f
emkay416 <carolinefobs3@gmail.com>
2026-04-12 13:24:44 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# MinisterX-md
-This is the official kijana ya minister Whatsapp bot working in all platforms. Hopefully you'll enjoy 🔥🔥
+ fork our repository [fork](https://github.com/kkeizza/Keith/fork)
+
+Pair site [pair](https://keithsite.top/keithpair)
+
+heroku deployment [heroku](https://keithsite.top/heroku)
+  
---
+++ settings.js
@@ -0,0 +1,67 @@
+
+const fs = require('fs-extra');
+const path = require('path');
+
+if (fs.existsSync('set.env')) {
+    require('dotenv').config({ path: __dirname + '/set.env' });
+}
+
+const session = process.env.SESSION || '';
+const dev = process.env.OWNER_NUMBER || '254748387615';
+
+const autostatusAutoviewStatus = process.env.AUTOVIEW_STATUS || 'true';
+const autostatusAutoLikeStatus = process.env.AUTOLIKE_STATUS || 'false';
+const autostatusAutoReplyStatus = process.env.AUTOREPLY_STATUS || 'false';
+const autostatusStatusReplyText = process.env.STATUS_REPLY_TEXT || '✅ Status Viewed By Keith Md';
+const autostatusStatusLikeEmojis = process.env.STATUS_LIKE_EMOJIS || '💛,❤️,💜,🤍,💙';
+
+const botPrefix = process.env.PREFIX || ".";
+const botAuthor = process.env.AUTHOR || "Keith";
+const botexpiration = process.env.BOT_EXPIRATION_DATE || "11/03/2027";
+const botUrl = process.env.BOT_PIC || "https://files.catbox.moe/9zqj7g.jpg";
+const botGurl = process.env.BOT_GURL || "https://github.com/Keithkeizzah/KEITH-MD";
+const botTimezone = process.env.BOT_TIMEZONE || "Africa/Nairobi";
+const botBotname = process.env.BOTNAME || "KEITH-MD";
+const botPackname = process.env.BOT_PACKNAME || "KEITH-MD";
+const botMode = process.env.BOT_MODE || "public";
+const botSessionName = process.env.BOT_SESSION_NAME || "keith-md";
+
+const { Sequelize } = require('sequelize'); 
+const DATABASE_URL = process.env.DATABASE_URL || './database.db'; 
+
+const database = DATABASE_URL === './database.db'
+    ? new Sequelize({
+        dialect: 'sqlite',
+        storage: DATABASE_URL,
+        logging: false,
+      })
+    : new Sequelize(DATABASE_URL, {
+        dialect: 'postgres',
+        ssl: true,
+        protocol: 'postgres',
+        dialectOptions: {
+          ssl: { require: true, rejectUnauthorized: false },
+        },
+        logging: false,
+      });
+
+module.exports = {  
+  database,
+  dev,
+  session, 
+  autostatusAutoviewStatus,
+  autostatusAutoLikeStatus,
+  autostatusAutoReplyStatus,
+  autostatusStatusReplyText,
+  autostatusStatusLikeEmojis,
+  botPrefix,
+  botAuthor,
+  botUrl,
+  botGurl,
+  botTimezone,
+  botBotname,
+  botexpiration,
+  botPackname,
+  botMode,
+  botSessionName
+};
